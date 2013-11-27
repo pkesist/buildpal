4b6121e9ddcadd583658fb864c160834e32594e8
Juraj Ivančić <juraj.ivancic@pke.hr>
2013-11-27 15:55:24 +0100

    Script (cx_Freeze) for creating manager installer.

---
+++ Source/setup_manager.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import site
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+site_packages = site.getsitepackages()
+
+possible_libzmq_locations = [os.path.join(s, 'zmq', 'libzmq.pyd') for s in site_packages]
+libzmq_pyd = None
+for p in (os.path.join(s, 'zmq', 'libzmq.pyd') for s in site_packages):
+    if os.path.exists(p):
+        libzmq_pyd = p
+        break
+if not libzmq_pyd:
+    raise Exception("Could not find 'libzmq.pyd'.")
+
+buildOptions = dict(packages = ['zmq.backend.cython', 'zmq.utils.garbage', 'Compilers'], excludes = ['zmq.libzmq'],
+include_files=[(libzmq_pyd, '')],
+)
+
+base = 'Console'
+
+executables = [
+    Executable('distribute_manager.py', base=base)
+]
+
+setup(name='DistriBuild_Manager',
+      version = '0.1',
+      description = 'Manager for DistriBuild.',
+      options = dict(build_exe = buildOptions),
+      executables = executables)

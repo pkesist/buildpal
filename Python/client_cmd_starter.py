9ac82beccc6dcf8588dda2f8f76ed7f5a6dc969e
Juraj Ivančić <juraj.ivancic@pke.hr>
2014-06-10 13:18:50 +0200

    print something when starting BuildPal console

--- Python/client_cmd_starter.py
+++ Python/client_cmd_starter.py
@@ -2,5 +2,5 @@ import sys
 
 if __name__ == '__main__':
     from buildpal.__main__ import main
-    main([sys.argv[0], 'client', '--run', 'cmd.exe', '/S', '/K'])
+    main([sys.argv[0], 'client', '--run', 'cmd.exe', '/Q', '/K', 'echo BuildPal Console'])
 

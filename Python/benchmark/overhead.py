0476323b641f155632e435137a393e4d7e856401
Juraj Ivančić <juraj.ivancic@pke.hr>
2014-06-24 12:43:59 +0200

    started benchmark scripts

---
+++ Python/benchmark/overhead.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import subprocess
+import time
+from multiprocessing import cpu_count
+
+def timeit(command):
+    times = {}
+
+    buildpal_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+    start_time = time.time()
+    subprocess.check_call(command, cwd=buildpal_dir)
+    times['regular'] = time.time() - start_time
+
+    server = subprocess.Popen([sys.executable, '-m', 'buildpal', 'manager', '--ui=none'])
+    manager = subprocess.Popen([sys.executable, '-m', 'buildpal', 'server', '--silent'])
+    time.sleep(5)
+    start_time = time.time()
+    subprocess.check_call([sys.executable, '-m', 'buildpal', 'client', '--run'] + command + ['--jobs={}'.format(2*cpu_count())], cwd=buildpal_dir)
+    times['buildpal_compiler_subst'] = time.time() - start_time
+    server.terminate()
+    manager.terminate()
+
+    server = subprocess.Popen([sys.executable, '-m', 'buildpal', 'manager', '--ui=none'])
+    manager = subprocess.Popen([sys.executable, '-m', 'buildpal', 'server', '--silent'])
+    time.sleep(5)
+    start_time = time.time()
+    subprocess.check_call([sys.executable, '-m', 'buildpal', 'client', '--no-cp', '--run'] + command + ['--jobs={}'.format(2*cpu_count())], cwd=buildpal_dir)
+    times['buildpal_no_createproc'] = time.time() - start_time 
+    server.terminate()
+    manager.terminate()
+    return times
+
+
+all_times = {}
+boost_times = all_times.setdefault('Boost', [])
+for x in range(5):
+    boost_times.append(timeit([sys.executable, 'setup.py', 'build_boost', '--complete-build', '--force', '--compiler=msvc']))
+
+with open('boost_times.dat', 'w') as file:
+    from pprint import pprint
+    pprint(boost_times, stream=file)
+
+
+
+

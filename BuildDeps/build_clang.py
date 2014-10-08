d1ec0204cd23fd1e51f787f6288cbf02c7267666
Juraj Ivančić <juraj.ivancic@pke.hr>
2014-10-08 22:56:27 +0200

    change clang build dir

--- BuildDeps/build_clang.py
+++ BuildDeps/build_clang.py
@@ -54,7 +54,7 @@ class build_clang(Command):
         self.x64 = None
         self.jobs = None
         self.clang_src_dir = 'llvm_clang_src'
-        self.clang_build_dir = 'llvm_clang_build'
+        self.clang_build_dir = 'llvm_clang_35_build_msvc_11'
         self.llvm_github_user = 'llvm-mirror'
         self.llvm_github_repo = 'llvm'
         self.llvm_github_branch = 'release_35'
